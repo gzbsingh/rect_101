91334725513c412ecb06a98563359fa77d3127af
gzbsingh <gentalman1945@gmail.com>
2022-05-18 04:47:06 +0530

    hii

---
+++ my-app/src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar=()=>{
+    return <div class="navbar_div">
+         <ul class="navbar">
+             <li><h2>LOGOBAKERY</h2></li>
+             <li><h2>Services</h2></li>
+             <li><h2>Projects</h2></li>
+             <li><h2>About</h2></li>
+             <li><h2><button>Connect</button></h2></li>
+         </ul>
+    </div>;
+}
+export default Navbar;
